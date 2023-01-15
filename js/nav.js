aa10cb1e2f5c10426ae1f06685e480f7772db780
Matias <matias.mesb@gmail.com>
2023-01-15 18:06:52 -0300

    Upload files

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,21 @@
 const nav = document.querySelector('.nav-menu');
+const dropdown = document.querySelector('.dropdown');
 
 const Nav = () => {
 
+    //Event listeners
     nav.addEventListener('click', changeIcon);
+    dropdown.addEventListener('click', openDropdown)
 
+    //Actions
     function changeIcon() {
         nav.classList.toggle('active');
     }
+
+    function openDropdown() {
+        dropdown.classList.toggle('active');
+    }
+
 };
 
 export default Nav;
